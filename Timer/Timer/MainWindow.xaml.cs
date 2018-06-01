614970f4e2325040f9df472d88ee53b7dd8efcb5
Diffyz <acerv3@lenta.ru>
2018-06-01 15:50:43 +0300

    create project

---
+++ Timer/Timer/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace TimerProject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DateTime date;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            date = DateTime.Now;
+           
+        }
+    }
+}
---
+++ TimerProject/TimerProject/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace TimerProject
+{
+
+    public partial class MainWindow : Window
+    {
+        DateTime date;
+        DispatcherTimer timer = new DispatcherTimer();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        
+        private void TimerStart(object sender, RoutedEventArgs e)
+        {
+            date = DateTime.Now;
+            timer.Tick += new EventHandler(TickTimer);
+            timer.Interval = TimeSpan.FromMilliseconds(10);
+            timer.Start();
+
+        }
+        private void TickTimer(object sender, EventArgs e)
+        {
+            long tick = DateTime.Now.Ticks - date.Ticks;
+            DateTime stopWatch = new DateTime();
+            stopWatch= stopWatch.AddTicks(tick);
+            _timer.Content = String.Format("{0:HH:mm:ss:ff}", stopWatch);
+           // _timer.Content = DateTime.Now.ToString("HH:mm:ss:ff");
+        }
+
+        private void TimerStop(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+    }
+}
